67ff7297381543e28ffad465078cbb9be9f8a89d
Sanjay <sanjay237@protonmail.com>
2020-03-21 15:46:29 +0530

    string encoding for pixbuf

--- game.py
+++ game.py
@@ -456,7 +456,7 @@ class Game():
 def svg_str_to_pixbuf(svg_string):
     """ Load pixbuf from SVG string """
     pl = GdkPixbuf.PixbufLoader.new_with_type('svg')
-    pl.write(svg_string)
+    pl.write(svg_string.encode())
     pl.close()
     pixbuf = pl.get_pixbuf()
     return pixbuf
